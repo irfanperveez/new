2ae638518dabda81e01b8fb810a1a85c4e2e841f
irfanperveez <ipervez@leeddev.io>
2023-06-15 06:36:34 -0700

    Connect with postgresssql, added up email, & +92 no

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const connectToMongo = require('./db');  //import external module into this file.(export of module are returned )
-const express = require('express')  
-// const cors=require('cors')
-connectToMongo();
-const app = express()    //Create a new express appplication in app instance
+const express = require('express');
+
+const db = require('./compile/db');   //import external module into this file.(export of module are returned )
 const port = process.env.PORT || 6000
 
+// Test DB
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err))
+
+const app = express();  //Create a new express appplication in app instance
 app.use(express.json()) //is used to add a middleware function to the application's request processing pipeline. This ensures that any incoming requests with a JSON payload will be parsed, and the resulting data will be available in req.body within the route handlers.
 // app.use(cors())
-app.use('/api/user', require('./routes/user'))
+
+app.use('/api/user', require('./routes/user'));
+
+
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  console.log(`backend listening at http://localhost:${port}`)
-})
+    console.log(`backend listening at http://localhost:${port}`)
+  })
--- routes/user.js
+++ routes/user.js
@@ -1,62 +1,72 @@
 const express = require('express');
-const User = require('../model/User');
 const router = express.Router();
+const User = require('../model/User');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'abc';
 
-
-// ROUTE 1: Create a user account by using: POST "/api/user/createuser". 
+// ROUTE 1: Create a user account using: POST "/api/user/createuser". 
 router.post('/createuser', async (req, res) => {
-    
-    try {
-      const user = await User.create({
-        name: req.body.name,
-        password: req.body.password,
-        phone_number: req.body.phone_number,   
-      });
-      const authtoken = jwt.sign({"name":user.name},JWT_SECRET);
-      res.json({ authtoken })
-  
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
-    }
-  })
-  
-  
-  // ROUTE 2: Authenticate a user using: POST "/api/user/login". 
-  router.post('/login', async (req, res) => {
-    let success = false;
 
-  
-    const { phone_number, password } = req.body;
     try {
-      let user = await User.findOne({ phone_number });
-      if (!user) {
-        success = false
-        return res.status(400).json({ error: "Please try to login with correct credentials" });
-      }
-  
-      if (!(password==user.password)) {
-        success = false
-        return res.status(400).json({ success, error: "Please try to login with correct credentials" });
-      }
-  
-      const data = {
-          user: {
-              id: user.id,
-              name:user.name,
-              phone_number:user.phone_number,
-            }
-      }
-      const authtoken = jwt.sign(data, JWT_SECRET);
-      success = true;
-      res.json({ success, authtoken,data })
-  
+        const user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            phoneNumber: req.body.phoneNumber,
+            email: req.body.email
+        });
+
+        console.log(user)
+        const authtoken = jwt.sign({ "name": user.name }, JWT_SECRET);
+        res.json({ authtoken })
+
     } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
     }
-  });
-  module.exports = router
+})
+
+
+// ROUTE 2: Authenticate a user using: POST "/api/user/login". 
+router.post('/login', async (req, res) => {
+    let success = false;
+    const { phoneNumber, password } = req.body;
+    try {
+        let user = await User.findOne({
+            where: {
+                phoneNumber: { [Op.like]: phoneNumber }
+            }
+        });
+        if (!user) {
+            success = false
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        if (!(password == user.password)) {
+            success = false
+            return res.status(400).json({ success, error: "Please try to login with correct password" });
+
+        }
+        if (!(email == user.email)) {
+            success = false
+            return res.status(400).json({ success, error: "Please try to login with correct password" });
+        }
+        const data = {
+            user: {
+                id: user.id,
+                name: user.name,
+                phoneNumber: user.phoneNumber,
+                email: user.email
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        success = true;
+        res.json({ success, authtoken, data })
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+module.exports = router;
   

19cda8af7c5da2b032249155a2fb686716c648b9
irfanperveez <ipervez@leeddev.io>
2023-06-19 05:32:25 -0700

    Password Encryption and authenticate through JWT and GEt user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const User = require('../model/User');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
@@ -7,18 +8,30 @@ var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'abc';
 
 // ROUTE 1: Create a user account using: POST "/api/user/createuser". 
-router.post('/createuser', async (req, res) => {
+router.post('/createuser', async (req, res, next) => {
+
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    
 
     try {
         const user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: hashedPassword,
             phoneNumber: req.body.phoneNumber,
             email: req.body.email
+            
+            
         });
+        const data={
+            "name":user.name,
+            "phoneNumber":user.phoneNumber,
+            "email": user.email,
+        }
+        
 
-        console.log(user)
-        const authtoken = jwt.sign({ "name": user.name }, JWT_SECRET);
+        const authtoken = jwt.sign( {data},JWT_SECRET);
         res.json({ authtoken })
 
     } catch (error) {
@@ -42,14 +55,14 @@ router.post('/login', async (req, res) => {
             success = false
             return res.status(400).json({ error: "Please try to login with correct credentials" });
         }
-        if (!(password == user.password)) {
+
+        const passwordCompare =await bcrypt.compare(password, user.password)
+        if (!passwordCompare) {
             success = false
             return res.status(400).json({ success, error: "Please try to login with correct password" });
 
-        }
-        if (!(email == user.email)) {
-            success = false
-            return res.status(400).json({ success, error: "Please try to login with correct password" });
+            
+
         }
         const data = {
             user: {
@@ -59,7 +72,7 @@ router.post('/login', async (req, res) => {
                 email: user.email
             }
         }
-        const authtoken = jwt.sign(data, JWT_SECRET);
+        const authtoken = jwt.sign("abc",JWT_SECRET);
         success = true;
         res.json({ success, authtoken, data })
 
@@ -67,6 +80,24 @@ router.post('/login', async (req, res) => {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
     }
+
+    
 });
+router.post("/profile", (req,res)=>{
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+       
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+})
+function verifyToken(req, res, next){
+   
+}
 module.exports = router;
-  
